19e65f4c45f4abc6f84ab42fac1704ccdcd4554c
Szymon Jakubiak <jakuszymo@gmail.com>
2026-01-17 15:17:36 +0100

    implementacja elementu diagonalnego

--- src/gauss.c
+++ src/gauss.c
@@ -21,63 +21,52 @@ int eliminate(Matrix *mat, Matrix *b)
 {
 	printf("\n\n");
 	printf("\n\n");
-    /**
-  	 * Tutaj należy umieścić właściwą implemntację.
-		 */
+    
 
 	for(int kol = 0; kol < mat->c; kol++)
 	{
 		
-		//szukamy rzędu z el głównym
-		double max_abs_w_rzedzie = 0;
-		int idx_rzedu_z_max = kol; 
-		for(int rzad = kol; rzad < mat->r; rzad++)
-		{
-			if(fabs(mat->data[rzad][kol]) > max_abs_w_rzedzie)
-			{
+//szukamy rzędu z el głównym (najwiekszy co do modułu od diagonalnego w dół)
+		double max_abs_w_rzedzie = 0.0;
+		int idx_rzedu_z_max = kol;
+		for (int rzad = kol; rzad < mat->r; rzad++) {
+			if (fabs(mat->data[rzad][kol]) > max_abs_w_rzedzie) {
 				idx_rzedu_z_max = rzad;
 				max_abs_w_rzedzie = fabs(mat->data[rzad][kol]);
 			}
 		}
-	//zamieniamy w b
-		double b_holder;
-		b_holder = b->data[0][idx_rzedu_z_max];
-		b->data[0][idx_rzedu_z_max] = b->data[0][kol];
-		b->data[0][kol] = b_holder;	
-	
 		
-	//zamieniamy, by el glowny był "na górze"
-		double row_temp_holder[mat->c];
-		//kopiujemy gorny rzad do tymczasowej zmiennej
-		for(int kol_temp = 0; kol_temp < mat->c; kol_temp++)
-		{
-			row_temp_holder[kol_temp] = mat->data[idx_rzedu_z_max][kol_temp];
-			//printf("%d: %f \n", kol_temp, mat->data[idx_rzedu_z_max][kol_temp]);
-		}
-		for(int kol_temp = 0; kol_temp < mat->c; kol_temp++)
-		{
-			mat->data[idx_rzedu_z_max][kol_temp] = mat->data[kol][kol_temp];
-		}
-		for(int kol_temp = 0; kol_temp < mat->c; kol_temp++)
-		{
-			mat->data[kol][kol_temp] = row_temp_holder[kol_temp];
-		}
-		
-		//printToScreen(mat);
-		//printf("test\n");
 
-	//dokonujemy zerowania obecnej kolumny
-		if(kol != mat->c - 1)
-		{
-			for(int rzad = kol + 1; rzad < mat->r; rzad++)
-			{
-				double jaka_czesc_rzedu_gluwnego_odejmujemy = mat->data[rzad][kol] / mat->data[kol][kol];
-				for(int kol_temp = 0; kol_temp < mat->c; kol_temp++)
-				{
-					mat->data[rzad][kol_temp] = mat->data[rzad][kol_temp] - mat->data[kol][kol_temp] * jaka_czesc_rzedu_gluwnego_odejmujemy;
-					
-					b->data[0][rzad] = b->data[0][rzad] - b->data[0][kol] * jaka_czesc_rzedu_gluwnego_odejmujemy;
+		// jesli maksymalny element jest zerowy -> macierz osobliwa
+		if (max_abs_w_rzedzie == 0.0) {
+			return 1; /* macierz osobliwa */
+		}
+//zmiana do wywołania konfliktu na test
+
+		// zamieniamy wiersze macierzy A (mat) tak, aby element glowny byl na gorze
+		if (idx_rzedu_z_max != kol) {
+			double *tmp_row = (double*) malloc(sizeof(double) * mat->c);
+			for (int i = 0; i < mat->c; ++i) tmp_row[i] = mat->data[idx_rzedu_z_max][i];
+			for (int i = 0; i < mat->c; ++i) mat->data[idx_rzedu_z_max][i] = mat->data[kol][i];
+			for (int i = 0; i < mat->c; ++i) mat->data[kol][i] = tmp_row[i];
+			free(tmp_row);
+
+			// zamiana odpowiadajacych wierszy wektora b (kolumna)
+			double b_tmp = b->data[kol][0];
+			b->data[kol][0] = b->data[idx_rzedu_z_max][0];
+			b->data[idx_rzedu_z_max][0] = b_tmp;
+		}
+
+		// dokonujemy zerowania obecnej kolumny
+		if (kol != mat->c - 1) {
+			for (int rzad = kol + 1; rzad < mat->r; rzad++) {
+				double factor = mat->data[rzad][kol] / mat->data[kol][kol];
+				/* aktualizujemy tylko kolumny od 'kol' do konca */
+				for (int kol_temp = kol; kol_temp < mat->c; kol_temp++) {
+					mat->data[rzad][kol_temp] -= mat->data[kol][kol_temp] * factor;
 				}
+				/* aktualizujemy wektor b (kolumna) raz dla tego rzedu */
+				b->data[rzad][0] -= b->data[kol][0] * factor;
 			}
 		}
 		//printToScreen(mat);
@@ -89,5 +78,6 @@ int eliminate(Matrix *mat, Matrix *b)
 	printToScreen(b);
 	
 	return 0;
+
 }
 
